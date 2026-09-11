332e5967a12132a4b5944248772b33782adc8feb
Jose Perez <floreztt1999@gmail.com>
2026-09-10 19:30:07 -0500

    New: Se agrega el telefono en el guardado y actualizado del estudiante

--- src/main/java/com/spring/activity/service/EstudianteService.java
+++ src/main/java/com/spring/activity/service/EstudianteService.java
@@ -33,6 +33,7 @@ public class EstudianteService {
         estudiante.setDocumento(request.documento());
         estudiante.setNombre(request.nombre());
         estudiante.setCorreo(request.correo());
+        estudiante.setTelefono(request.telefono());
         estudiante.setProgramaAcademico(programaAcademico);
 
         Estudiante estudianteGuardado = estudianteRepository.save(estudiante);
@@ -58,6 +59,7 @@ public class EstudianteService {
         estudiante.setNombre(request.nombre());
         estudiante.setDocumento(request.documento());
         estudiante.setCorreo(request.correo());
+        estudiante.setTelefono(request.telefono());
 
         Estudiante estudianteActualizado = estudianteRepository.save(estudiante);
         return convertirAResponse(estudianteActualizado);
@@ -77,6 +79,7 @@ public class EstudianteService {
                 estudiante.getId(),
                 estudiante.getNombre(),
                 estudiante.getDocumento(),
+                estudiante.getTelefono(),
                 estudiante.getProgramaAcademico().getId(),
                 estudiante.getProgramaAcademico().getNombre());
     }
